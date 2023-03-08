4b17fa1d6ea84e9d57ac0a4f5ec327170da4e7f5
puri0074 <1472315@senati.pe>
2023-03-07 19:08:43 -0500

    lista en python

---
+++ lista.py
@@ -0,0 +1,6 @@
+#desarrolle un algoritmo ue escriba un codigo de programacion donde me permita imprimir el primero y el ultimo valor de la lista
+list=['papaya', 'orange', 'apple', 'pear', 'grape']
+x= list[0]
+y= list[-1]
+print(x)
+print(y)
